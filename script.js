84461274206053ba6ca151a5f7f7a3743804428e
yossefAlatter <yossefalatter@gmail.com>
2020-09-30 12:08:34 +0200

    nice-position-using

---
+++ script.js
@@ -0,0 +1,22 @@
+var next = document.querySelectorAll(".next");
+var prev = document.querySelectorAll(".prev");
+var john = document.querySelector(".john");
+var tanya = document.querySelector(".tanya");
+console.log(john.getAttribute("display"));
+next.forEach(function(item){
+    if(true){
+        item.onclick=()=>{    
+            john.style.display="block";
+            tanya.style.display="none";
+    }
+    }else{
+        john.style.display="none";
+        tanya.style.display="block";
+    }
+});
+prev.forEach(function(item){
+    item.onclick=()=>{    
+        john.style.display="none";
+        tanya.style.display="block";
+}
+});
